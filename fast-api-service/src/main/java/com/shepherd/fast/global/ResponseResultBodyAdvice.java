9db6c28c70a6e59832680f2b619d56bdd71b1519
fjzheng <1059959730@qq.com>
2022-06-17 11:02:33 +0800

    fix: :bug: 修复接口返回为类型为String时，如果返回参数为null导致统计结果封装操作报错

--- fast-api-service/src/main/java/com/shepherd/fast/global/ResponseResultBodyAdvice.java
+++ fast-api-service/src/main/java/com/shepherd/fast/global/ResponseResultBodyAdvice.java
@@ -6,20 +6,16 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.core.annotation.AnnotatedElementUtils;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
-import org.springframework.web.util.WebUtils;
 
 import javax.annotation.Resource;
 import java.lang.annotation.Annotation;
+import java.util.Objects;
 
 /**
  * @author fjzheng
@@ -49,7 +45,9 @@ public class ResponseResultBodyAdvice implements ResponseBodyAdvice<Object> {
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
         //如果返回类型是string，那么springmvc是直接返回的，此时需要手动转化为json
-        if (body instanceof String) {
+        // 当body都为null时，下面的if判断条件都不满足，如果接口返回类似为String，会报错com.shepherd.fast.global.ResponseVO cannot be cast to java.lang.String
+        Class<?> returnClass = returnType.getMethod().getReturnType();
+        if (body instanceof String || Objects.equals(returnClass, String.class)) {
             String value = objectMapper.writeValueAsString(ResponseVO.success(body));
             return value;
         }
