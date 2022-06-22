e8378ade50ac1d3e51c15bb8066bf3ac12fbb387
fjzheng <1059959730@qq.com>
2022-06-22 18:57:16 +0800

    update: :art: 支持序列化

--- fast-api-service/src/main/java/com/shepherd/fast/entity/DataSource.java
+++ fast-api-service/src/main/java/com/shepherd/fast/entity/DataSource.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -13,7 +14,7 @@ import java.util.Date;
  */
 
 @Data
-public class DataSource {
+public class DataSource implements Serializable {
 
     @TableId(type = IdType.AUTO)
     private Long id;
