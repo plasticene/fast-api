bc1624dc8463edeecb6b8164cbedc29d844e8d05
fjzheng <1059959730@qq.com>
2022-06-22 18:58:14 +0800

    add: :art: 添加redisTemplate配置bean

---
+++ fast-api-service/src/main/java/com/shepherd/fast/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.shepherd.fast.config;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+
+import java.net.UnknownHostException;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/6/22 10:01
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory)
+            throws UnknownHostException {
+        RedisTemplate<Object, Object> template = new RedisTemplate<Object, Object>();
+        template.setConnectionFactory(redisConnectionFactory);
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        template.setDefaultSerializer(serializer);
+        return template;
+    }
+}
