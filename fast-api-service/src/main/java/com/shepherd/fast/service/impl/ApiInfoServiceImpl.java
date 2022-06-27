14a7458d1ba86d11209bbb981a97a9672f53df7e
fjzheng <1059959730@qq.com>
2022-06-27 10:30:52 +0800

    add: :sparkles: 添加api管理的dao和service

---
+++ fast-api-service/src/main/java/com/shepherd/fast/service/impl/ApiInfoServiceImpl.java
@@ -0,0 +1,13 @@
+package com.shepherd.fast.service.impl;
+
+import com.shepherd.fast.service.ApiInfoService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/6/27 10:29
+ */
+@Service
+public class ApiInfoServiceImpl implements ApiInfoService {
+}
