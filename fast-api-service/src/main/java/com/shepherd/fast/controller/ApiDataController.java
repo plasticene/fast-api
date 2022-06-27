f6b83325a8f07f2711a143a31037c1092c9ff484
fjzheng <1059959730@qq.com>
2022-06-27 10:22:39 +0800

    add: :sparkles: 初步添加api接口数据获取功能

---
+++ fast-api-service/src/main/java/com/shepherd/fast/controller/ApiDataController.java
@@ -0,0 +1,16 @@
+package com.shepherd.fast.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/6/24 17:29
+ */
+@RestController
+@RequestMapping("/api/data")
+@Api(tags = "api数据")
+public class ApiDataController {
+}
