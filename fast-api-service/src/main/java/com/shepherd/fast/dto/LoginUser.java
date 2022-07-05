764f475af4560a39ae5a97703de5617551b17d51
fjzheng <1059959730@qq.com>
2022-07-05 10:52:43 +0800

    add: :sparkles: 初步添加登陆用户信息

---
+++ fast-api-service/src/main/java/com/shepherd/fast/dto/LoginUser.java
@@ -0,0 +1,14 @@
+package com.shepherd.fast.dto;
+
+import lombok.Data;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/7/5 10:08
+ */
+@Data
+public class LoginUser {
+    private Long Id;
+    private String name;
+}
