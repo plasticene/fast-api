20fa1fd1cb63cd7c7d0119a4834a733ebee26edb
fjzheng <1059959730@qq.com>
2022-07-05 10:54:19 +0800

    add: :sparkles: 注入新版本的分页拦截器和字段填充bean

---
+++ fast-api-service/src/main/java/com/shepherd/fast/config/MPConfig.java
@@ -0,0 +1,30 @@
+package com.shepherd.fast.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.shepherd.fast.global.DefaultDBFieldHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/6/17 10:52
+ * MP → mybatis-plus
+ */
+@Configuration
+public class MPConfig {
+
+    @Bean
+    public PaginationInnerInterceptor paginationInterceptor() {
+        //必须注入这个分页插件拦截器，否则分页不成功
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        return paginationInnerInterceptor;
+    }
+
+    @Bean
+    public MetaObjectHandler defaultMetaObjectHandler(){
+        return new DefaultDBFieldHandler(); // 自动填充参数类
+    }
+}
