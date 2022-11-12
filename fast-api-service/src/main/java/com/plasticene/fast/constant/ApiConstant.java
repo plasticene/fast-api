185b3152a4173ebb4a56791865ac031122dade09
fjzheng <1059959730@qq.com>
2022-11-12 23:53:02 +0800

    add: :rocket: 添加api相关常量

---
+++ fast-api-service/src/main/java/com/plasticene/fast/constant/ApiConstant.java
@@ -0,0 +1,14 @@
+package com.plasticene.fast.constant;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/11/11 11:25
+ */
+public interface ApiConstant {
+
+    Integer API_STATUS_DRAFT = 0;
+    Integer API_STATUS_RELEASE = 1;
+    Integer API_STATUS_CHANGE = 2;
+    Integer API_STATUS_OFFLINE = 3;
+}
