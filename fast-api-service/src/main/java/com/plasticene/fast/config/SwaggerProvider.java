d85072faa838c7340012f30252f67e6fca579557
fjzheng <1059959730@qq.com>
2022-11-12 23:59:23 +0800

    update: :art: 注释代码，解决knife4j调试接口是没有字段

--- fast-api-service/src/main/java/com/plasticene/fast/config/SwaggerProvider.java
+++ fast-api-service/src/main/java/com/plasticene/fast/config/SwaggerProvider.java
@@ -1,35 +1,35 @@
-package com.plasticene.fast.config;
-
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
-import springfox.documentation.swagger.web.SwaggerResource;
-import springfox.documentation.swagger.web.SwaggerResourcesProvider;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * @author fjzheng
- * @version 1.0
- * @date 2022/11/10 17:05
- */
-@Component
-@Primary
-public class SwaggerProvider implements SwaggerResourcesProvider {
-    @Override
-    public List<SwaggerResource> get() {
-        List<SwaggerResource> resources = new ArrayList<>();
-        resources.add(swaggerResource("应用接口", "/v2/api-docs", "1.0"));
-        resources.add(swaggerResource("Dataway接口", "/api/docs", "1.0"));
-        return resources;
-    }
-
-    private SwaggerResource swaggerResource(String name, String location, String version) {
-        SwaggerResource swaggerResource = new SwaggerResource();
-        swaggerResource.setName(name);
-        swaggerResource.setLocation(location);
-        swaggerResource.setSwaggerVersion(version);
-        return swaggerResource;
-    }
-}
-
+//package com.plasticene.fast.config;
+//
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.stereotype.Component;
+//import springfox.documentation.swagger.web.SwaggerResource;
+//import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * @author fjzheng
+// * @version 1.0
+// * @date 2022/11/10 17:05
+// */
+//@Component
+//@Primary
+//public class SwaggerProvider implements SwaggerResourcesProvider {
+//    @Override
+//    public List<SwaggerResource> get() {
+//        List<SwaggerResource> resources = new ArrayList<>();
+//        resources.add(swaggerResource("应用接口", "/v2/api-docs", "1.0"));
+//        resources.add(swaggerResource("Dataway接口", "/api/docs", "1.0"));
+//        return resources;
+//    }
+//
+//    private SwaggerResource swaggerResource(String name, String location, String version) {
+//        SwaggerResource swaggerResource = new SwaggerResource();
+//        swaggerResource.setName(name);
+//        swaggerResource.setLocation(location);
+//        swaggerResource.setSwaggerVersion(version);
+//        return swaggerResource;
+//    }
+//}
+//
