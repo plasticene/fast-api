628f4ce8aaa60ab944fc86990fe05225f98f1d0c
fjzheng <1059959730@qq.com>
2022-11-14 18:34:53 +0800

    add: :rocket: 添加应用管理功能

---
+++ fast-api-service/src/main/java/com/plasticene/fast/controller/ApplicationController.java
@@ -0,0 +1,16 @@
+package com.plasticene.fast.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/11/14 10:17
+ */
+@RestController
+@Api(tags = "应用管理")
+@RequestMapping("/app")
+public class ApplicationController {
+}
