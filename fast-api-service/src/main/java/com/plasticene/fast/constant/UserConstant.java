6be11e8d0a7bc9ae1eb9c3e4b9684cf3f54f0cd7
fjzheng <1059959730@qq.com>
2023-01-04 11:33:02 +0800

    add: :rocket: 添加常量

---
+++ fast-api-service/src/main/java/com/plasticene/fast/constant/UserConstant.java
@@ -0,0 +1,10 @@
+package com.plasticene.fast.constant;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2023/1/3 22:29
+ */
+public interface UserConstant {
+    String LOGIN_KEY_PREFIX = "token_";
+}
